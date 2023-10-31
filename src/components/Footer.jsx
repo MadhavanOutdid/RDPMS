b6481af062f73dd2b52f26a4281f5c8f86cafea7
vivek-outdid <vivek@outdidtech.com>
2023-10-31 16:14:08 +0530

    Revert "React Footer update"
    This reverts commit 5b027ed968ab6024092c1579642ad8f5161a12c0.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
 function Footer() {
-  const year = new Date();
   return (
     // <!-- Main Footer -->
       <footer className="main-footer" >
-          <strong>Copyright &copy; {year.getFullYear()} <a href="https://www.sensedgetss.com/">sensedge</a>.</strong> All rights reserved.
+          <strong>Copyright © 2023 <a href="https://www.sensedgetss.com/">sensedge</a>.</strong> All rights reserved.
       </footer>
         
   );
