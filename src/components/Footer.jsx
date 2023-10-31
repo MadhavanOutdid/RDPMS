5b027ed968ab6024092c1579642ad8f5161a12c0
KesavanOutdid <kesavan@outdidtech.com>
2023-10-31 10:15:19 +0530

    React Footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 function Footer() {
+  const year = new Date();
   return (
     // <!-- Main Footer -->
       <footer className="main-footer" >
-          <strong>Copyright © 2023 <a href="https://www.sensedgetss.com/">sensedge</a>.</strong> All rights reserved.
+          <strong>Copyright &copy; {year.getFullYear()} <a href="https://www.sensedgetss.com/">sensedge</a>.</strong> All rights reserved.
       </footer>
         
   );
